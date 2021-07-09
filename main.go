5e35eebb958192a077006e36c53e1a6c79ea6428
Joseph Garcia <joseph@krakenhub.net>
2021-07-09 11:45:05 -0500

    primer commit, boilerplate del bot

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"net/http"
+	"os"
+	"log"
+	"github.com/joho/godotenv"
+	"github.com/go-telegram-bot-api/telegram-bot-api"
+)
+
+func main() {
+	err := godotenv.Load()
+	tgToken := os.Getenv("TELEGRAM_BOT_TOKEN")
+	MainUrl := os.Getenv("MAIN_URL")
+
+
+	bot, err := tgbotapi.NewBotAPI(tgToken)
+	if err != nil {
+		log.Printf("$TELEGRAM_BOT_TOKEN Must Be Set")
+		log.Printf(tgToken);
+		log.Fatal(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	_, err = bot.SetWebhook(tgbotapi.NewWebhookWithCert(MainUrl+bot.Token, "cert.pem"))
+	if err != nil {
+		log.Fatal("error al setear webhook")
+		log.Fatal(err)
+	}
+	info, err := bot.GetWebhookInfo()
+	if err != nil {
+		log.Fatal("error al obtener info de webhook")
+		log.Fatal(err)
+	}
+	if info.LastErrorDate != 0 {
+		log.Printf("Telegram callback failed: %s", info.LastErrorMessage)
+	}
+	updates := bot.ListenForWebhook("/" + bot.Token)
+	go http.ListenAndServeTLS("localhost:3000", "cert.pem", "key.pem", nil)
+
+	for update := range updates {
+		log.Printf("%+v\n", update)
+	}
+}
